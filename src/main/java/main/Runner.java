7e75d9b048ec53e8bd101eee70aade05654a8f50
Elizabeth Lewis <elewis@academytrainee.com>
2020-09-09 16:57:35 +0100

    Made changes to Runner.java

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -9,16 +9,49 @@ public class Runner {
 		sc = new Scanner(System.in);
 		boolean loop = true;
 		while (loop) {
-			System.out.println("1. Librarian\n2.Customer\n3.Exit");
+			System.out.println("1. Modify Items\n2.Modify People\n3.Exit");
 			String input = sc.nextLine();
 			switch (input.toUpperCase()) {
-			case "LIBRARIAN" : break;
-			case "CUSTOMER" : break;
-			case "EXIT" : break;
+			case "ITEMS" : items(); break;
+			case "PEOPLE" : people(); break;
+			case "EXIT" : loop = false; break;
 			default : System.out.println("Can't interpret. Please try again.\n");
 			}
 		}
 
 	}
-
+	
+	private static void items() {
+		boolean loop = true;
+		while (loop) {
+			System.out.println("Choices:\n"
+					+ "1.INSERT\n"
+					+ "2.UPDATE\n"
+					+ "3.DELETE\n");
+			String input = sc.nextLine();
+			switch (input.toUpperCase()) {
+				case "INSERT" : break;
+				case "UPDATE" : break;
+				case "DELETE" : break;
+				default : System.out.println("Can't interpret. Please try again.\n");
+			}
+		}
+	}
+	
+	private static void people() {
+		boolean loop = true;
+		while (loop) {
+			System.out.println("Choices:\n"
+					+ "1.INSERT\n"
+					+ "2.UPDATE\n"
+					+ "3.DELETE\n");
+			String input = sc.nextLine();
+			switch (input.toUpperCase()) {
+				case "INSERT" : break;
+				case "UPDATE" : break;
+				case "DELETE" : break;
+				default : System.out.println("Can't interpret. Please try again.\n");
+			}
+		}
+	}
 }
